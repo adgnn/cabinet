8a69e1ed480175c46a4bba7f6558e61e380f8bfe
huang <1323178720@qq.com>
2019-04-04 20:05:46 +0800

    user分支

---
+++ index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import App from './src/views/app.vue';
+
+
+import {get, post, put, deleteDate,success, fail} from './utils/http.js'
+
+Vue.prototype.$get = get;
+Vue.prototype.$post = post;
+Vue.prototype.$put = put;
+Vue.prototype.$delete = deleteDate;
+Vue.prototype.$success = success;
+Vue.prototype.$fail = fail;
+
+// import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(ElementUI);
+
+
+import createVuex from './store/index.js';
+import createRouter from './vue-router/router.js'
+
+Vue.use(Vuex);
+Vue.use(VueRouter);
+const router = createRouter();
+const store = createVuex();
+
+new Vue({
+    el: "#root",
+    router,
+    store,
+    render: (h) => h(App)
+});
+
+
+
